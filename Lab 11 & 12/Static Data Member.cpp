337f29b5e3959b0ee9a9f6103396ee3371ac0f5a
Syed Fasih-ul-Hassan Gardezi <fasihulh95@gmail.com>
2026-09-19 14:54:44 +0500

    Add files via upload

---
+++ Lab 11 & 12/Static Data Member.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+# include <string>
+using namespace std;
+class BankAccount{
+	private:
+	string accountHolderName;
+	double balance;
+	
+	public:
+		
+		BankAccount(string n, double b){
+			accountHolderName = n;
+			balance = b;
+		}
+		
+		void display(){
+			cout << "---<(Account Details)>---" << endl;
+			cout << "Account Holder: " << accountHolderName << endl;
+			cout << "Total Balance: " << balance << endl;
+			
+		}
+		
+		static int totalAccounts;		
+};
+
+int BankAccount :: totalAccounts = 02;
+
+int main(){
+	
+	BankAccount a1("Ali Raza", 5000);
+	cout << "Account No 01:" << endl;
+	a1.display();
+	cout << endl;
+	BankAccount a2("Muhammad Abdullah", 10000);
+	cout << "Account No 02: " << endl;
+	a2.display();
+	cout << endl;
+	cout << "|---------------------------------------|" << endl;
+	cout << "   Total Accounts Created Sofar: " << BankAccount:: totalAccounts<< endl;
+	cout << "|---------------------------------------|" << endl;
+	return 0;
+}

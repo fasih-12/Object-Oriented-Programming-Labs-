0ebca7fd98148225fda2066b399f551eca56b95d
Syed Fasih-ul-Hassan Gardezi <fasihulh95@gmail.com>
2026-09-19 14:48:48 +0500

    Add files via upload

---
+++ Lab 6/File Handling System.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class File{
+	public:
+		File(){
+			cout << "File opened" << endl;
+		}
+		
+		virtual ~File(){
+			cout << "File closed" << endl;
+		}
+};
+
+class TextFile : public File{
+	public:
+		TextFile(){
+			cout << "TextFile opened" << endl;
+			
+		}
+		
+		~TextFile(){
+			cout << "TextFile closed" << endl;
+		}
+};
+
+int main(){
+	TextFile tf;
+	return 0;
+}
